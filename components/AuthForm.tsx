cd34ad6f82350d8e859abc3bf823a094cf435dfb
saikishan8055 <saikishan.sai01@gmail.com>
2026-01-11 02:17:16 +0530

    Auth and redirect to homepage

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,140 +1,302 @@
+// "use client";
+// import * as z from "zod";
+
+// import React from "react";
+// import { zodResolver } from "@hookform/resolvers/zod";
+// import { useForm } from "react-hook-form";
+// import { Button } from "@/components/ui/button";
+// import Image from "next/image";
+// import {
+//   Form,
+//   FormControl,
+//   FormDescription,
+//   // FormField,
+//   FormItem,
+//   FormLabel,
+//   FormMessage,
+// } from "@/components/ui/form";
+// import FormField from "../components/FormField";
+// import { Input } from "@/components/ui/input";
+// import Link from "next/link";
+// import { toast } from "sonner";
+// import { useRouter } from "next/navigation";
+// import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+// import { auth } from "./firebase/client";
+// import { SignIn, SignUp } from "@/lib/actions/auth.actions";
+
+// const authFormSchema =(type:FormType)=>{
+//   return z.object({
+//     name:type ==='sign-up'?z.string().min(3):z.string().optional(),
+//     email:z.string().email(),
+//     password : z.string().min(3),
+
+//   })
+
+// }
+
+// const AuthForm = ({type}:{type:FormType}) => {
+//   const router = useRouter()
+//   const formSchema = authFormSchema(type)
+//   const form = useForm<z.infer<typeof formSchema>>({
+//     resolver: zodResolver(formSchema),
+//     defaultValues: {
+//       name: "",
+//       email:'',
+//       password:''
+
+
+//     },
+//   });
+
+//   // 2. Define a submit handler.
+//   async function onSubmit(values: z.infer<typeof formSchema>) {
+//  try{
+//   if(type === 'sign-up'){
+//     const {name,email,password} = values;
+//     const userCrediential = await createUserWithEmailAndPassword(auth,email,password);
+//     const result = await SignUp({
+//       uid:userCrediential.user.uid,
+//       name:name!,
+//       email,
+//       password,
+//     })
+//     if (result?.success){
+//       toast.error(result?.message);
+//       result
+//     }else{
+//       const {email,password} = values;
+//       const userCredentail = await signInWithEmailAndPassword(auth,email,password);
+//       const idToken =await userCredentail.user.getIdToken();{
+//         if(!idToken){
+//           toast.error('Sign in failed');
+//           return
+//         }
+//         await SignIn({
+//           email,idToken
+//         })
+//       }
+
+//     }
+//   toast.success('Account created successfully . Please sign in')
+//   router.push('/sign-in')
+
+//   }else{
+// toast.success('Sign in Successfully')
+// router.push('/')
+//   }
+
+//  }catch(error){
+//       console.log(error);
+//       toast.error(`There is an error :${error}`)
+
+
+//  }
+//   }
+//   const isSignIn = type==='sign-in';
+//   return (
+//     <div className="card-border lg:min-w-[566px]">
+//       <div className="flex flex-col gap-6 card py-14 px-10">
+//         <div className="flex flex-row gap-2 justify-center">
+//           <Image src="/logo.svg" alt="logo" height={32} width={38} />
+//           <h1 className="text-primary-100">PrepareForInterview</h1>
+
+//         </div>
+//       <h3 className="text-primary-100">Practice Job Interview With AI</h3>
+
+//       <Form {...form}>
+//         <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-6 mt-4 form">
+//          {!isSignIn && 
+//          (
+//           <FormField control={form.control}
+//               name='name'
+//               label="Name"
+//               placeholder="Your Name" type={"text"}/>
+//          )
+         
+//          }
+//  <FormField control={form.control}
+//               name='email'
+//               label="Email"
+//               placeholder="Your email address " type={"email"}/>         
+//               <FormField control={form.control}
+//               name='password'
+//               label="Password"
+//               placeholder="Enter your password " type={"password"}/> 
+
+//           <Button className="btn"type="submit">{isSignIn? 'sign-in':'Create an account'}</Button>
+//         </form>
+//       </Form>
+//       <p>{isSignIn? 'No account yet':'Have an account already'}
+// <Link href={!isSignIn?'sign-in':'sign-up'}
+//  className="font-bold text-user-primary ml-1">
+// {!isSignIn?'Sign in':'Sign up'}
+// </Link>
+// </p>
+//     </div>
+//           </div>
+
+//   );
+// };
+
+// export default AuthForm;
+
 "use client";
-import * as z from "zod";
 
-import React from "react";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  // FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import FormField from "../components/FormField";
-import { Input } from "@/components/ui/input";
+import { z } from "zod";
 import Link from "next/link";
+import Image from "next/image";
 import { toast } from "sonner";
-import { useRouter } from "next/navigation";
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "./firebase/client";
-import { SignIn, SignUp } from "@/lib/actions/auth.actions";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-const authFormSchema =(type:FormType)=>{
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+
+import { Form } from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+
+import { SignIn,SignUp } from "../lib/actions/auth.actions";
+import FormField from "./FormField";
+
+const authFormSchema = (type: FormType) => {
   return z.object({
-    name:type ==='sign-up'?z.string().min(3):z.string().optional(),
-    email:z.string().email(),
-    password : z.string().min(3),
+    name: type === "sign-up" ? z.string().min(3) : z.string().optional(),
+    email: z.string().email(),
+    password: z.string().min(3),
+  });
+};
 
-  })
+const AuthForm = ({ type }: { type: FormType }) => {
+  const router = useRouter();
 
-}
-
-const AuthForm = ({type}:{type:FormType}) => {
-  const router = useRouter()
-  const formSchema = authFormSchema(type)
+  const formSchema = authFormSchema(type);
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       name: "",
-      email:'',
-      password:''
-
-
+      email: "",
+      password: "",
     },
   });
 
-  // 2. Define a submit handler.
-  async function onSubmit(values: z.infer<typeof formSchema>) {
- try{
-  if(type === 'sign-up'){
-    const {name,email,password} = values;
-    const userCrediential = await createUserWithEmailAndPassword(auth,email,password);
-    const result = await SignUp({
-      uid:userCrediential.user.uid,
-      name:name!,
-      email,
-      password,
-    })
-    if (result?.success){
-      toast.error(result?.message);
-      result
-    }else{
-      const {email,password} = values;
-      const userCredentail = await signInWithEmailAndPassword(auth,email,password);
-      const idToken =await userCredentail.user.getIdToken();{
-        if(!idToken){
-          toast.error('Sign in failed');
-          return
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
+    try {
+      if (type === "sign-up") {
+        const { name, email, password } = data;
+
+        const userCredential = await createUserWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+
+        const result = await SignUp({
+          uid: userCredential.user.uid,
+          name: name!,
+          email,
+          password,
+        });
+
+        if (!result.success) {
+          toast.error(result.message);
+          return;
         }
+
+        toast.success("Account created successfully. Please sign in.");
+        router.push("/sign-in");
+      } else {
+        const { email, password } = data;
+
+        const userCredential = await signInWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+
+        const idToken = await userCredential.user.getIdToken();
+        if (!idToken) {
+          toast.error("Sign in Failed. Please try again.");
+          return;
+        }
+
         await SignIn({
-          email,idToken
-        })
+          email,
+          idToken,
+        });
+
+        toast.success("Signed in successfully.");
+        router.push("/");
       }
-
-    }
-  toast.success('Account created successfully . Please sign in')
-  router.push('/sign-in')
-
-  }else{
-toast.success('Sign in Successfully')
-router.push('/')
-  }
-
- }catch(error){
+    } catch (error) {
       console.log(error);
-      toast.error(`There is an error :${error}`)
+      toast.error(`There was an error: ${error}`);
+    }
+  };
 
+  const isSignIn = type === "sign-in";
 
- }
-  }
-  const isSignIn = type==='sign-in';
   return (
     <div className="card-border lg:min-w-[566px]">
       <div className="flex flex-col gap-6 card py-14 px-10">
         <div className="flex flex-row gap-2 justify-center">
           <Image src="/logo.svg" alt="logo" height={32} width={38} />
-          <h1 className="text-primary-100">PrepareForInterview</h1>
-
+          <h2 className="text-primary-100">PrepWise</h2>
         </div>
-      <h3 className="text-primary-100">Practice Job Interview With AI</h3>
 
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-6 mt-4 form">
-         {!isSignIn && 
-         (
-          <FormField control={form.control}
-              name='name'
-              label="Name"
-              placeholder="Your Name" type={"text"}/>
-         )
-         
-         }
- <FormField control={form.control}
-              name='email'
+        <h3>Practice job interviews with AI</h3>
+
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="w-full space-y-6 mt-4 form"
+          >
+            {!isSignIn && (
+              <FormField
+                control={form.control}
+                name="name"
+                label="Name"
+                placeholder="Your Name"
+                type="text"
+              />
+            )}
+
+            <FormField
+              control={form.control}
+              name="email"
               label="Email"
-              placeholder="Your email address " type={"email"}/>         
-              <FormField control={form.control}
-              name='password'
+              placeholder="Your email address"
+              type="email"
+            />
+
+            <FormField
+              control={form.control}
+              name="password"
               label="Password"
-              placeholder="Enter your password " type={"password"}/> 
+              placeholder="Enter your password"
+              type="password"
+            />
 
-          <Button className="btn"type="submit">{isSignIn? 'sign-in':'Create an account'}</Button>
-        </form>
-      </Form>
-      <p>{isSignIn? 'No account yet':'Have an account already'}
-<Link href={!isSignIn?'sign-in':'sign-up'}
- className="font-bold text-user-primary ml-1">
-{!isSignIn?'Sign in':'Sign up'}
-</Link>
-</p>
+            <Button className="btn" type="submit">
+              {isSignIn ? "Sign In" : "Create an Account"}
+            </Button>
+          </form>
+        </Form>
+
+        <p className="text-center">
+          {isSignIn ? "No account yet?" : "Have an account already?"}
+          <Link
+            href={!isSignIn ? "/sign-in" : "/sign-up"}
+            className="font-bold text-user-primary ml-1"
+          >
+            {!isSignIn ? "Sign In" : "Sign Up"}
+          </Link>
+        </p>
+      </div>
     </div>
-          </div>
-
   );
 };
 
--- lib/actions/auth.actions.ts
+++ lib/actions/auth.actions.ts
@@ -1,110 +1,244 @@
-'use server';
+// 'use server';
+
+// import { db ,auth} from "@/components/firebase/admin";
+// import { get } from "http";
+// import { cookies } from "next/headers";
+// import { success } from "zod";
+// import { de, id } from "zod/v4/locales";
+// const ONE_WEEK = 60*60*24*7
+
+// export async function SignUp(params:SignUpParams){
+//     const {uid,name,email}=params;
+//     try {
+//         const userRecord = await db.collection('users').doc(uid).get()
+//         if(userRecord.exists){
+//             return{
+//                 success:false,
+//                 message:'User already exists.Please sign in instead'
+//             }
+
+
+//         }
+//         await db.collection('users').doc(uid).set({
+//             name,email
+//         })
+//         return{
+//             success:true,
+//             message:'Account created successfully .Please Sign In '
+//         }
+
+//     }catch(e:any){
+//         console.error('Error creating a user',e);
+//         if(e.code === 'auth/email-already-exists'){
+//             return{
+//                 succes:false,
+//                 message:'This E-mail is already in use'
+//             }
+
+//         }
+//         return{
+//             success:false,
+//             message:'Failed to create an account'
+//         }
+
+//     }
+    
+// }
+// export async function SignIn(params:SignInParams){
+//     const {email,idToken} = params
+//     try{
+//         const userRecord = await auth.getUserByEmail(email)
+//         if(!userRecord){
+//             return{
+//                 success:false,
+//                 message : 'User does not exist.Create an account instsead'
+
+//             }
+//         }
+//         await setSessionCookies(idToken);
+//     }catch(e){
+//         console.log(e);
+//         return{
+//             success:false,
+//             message:'failed to login into the account'
+//         }
+//     }
+
+// }
+// export async function setSessionCookies(idToken : string){
+//     const cookieStore = await   cookies()
+//     const sessionCookies = await auth.createSessionCookie(idToken,{
+//         expiresIn:ONE_WEEK*1000,
+//     })
+//     cookieStore.set('session',sessionCookies,{
+//         maxAge:ONE_WEEK,
+//         httpOnly:true,
+//         secure:process.env.NODE_ENV === 'production',
+//         path:'/',
+//         sameSite:'lax'
+         
+//     })
+
+// }
+// export async function getCurrentuser(): Promise<User|null>{
+//     const cookieStore = await cookies();
+//     console.log("SESSION COOKIE:", cookieStore.get("session"));
+
+//     const sessionCookies = cookieStore.get('session')?.value;
+//     if(!sessionCookies) return null;
+//     try{
+//         const decodeClaims = await auth.verifySessionCookie(sessionCookies,true);
+//         const userRecord = await db
+//         .collection('users')
+//         .doc(decodeClaims.uid)
+//         .get()
+//         if(!userRecord.exists) return null
+//         return {
+//             ...userRecord.data(),
+//             id:userRecord.id,
+//         }as User;
+
+
+//     }catch(e){
+//         console.log(e)
+//         return null
+//     }
+
+// }
+// export async function isAuthenticated(){
+//     const user = await getCurrentuser()
+//     return !!user;
+// }
+"use server";
 
 import { db ,auth} from "@/components/firebase/admin";
-import { CollectionReference, DocumentData, DocumentReference } from "firebase-admin/firestore";
-import { get } from "http";
 import { cookies } from "next/headers";
-import { success } from "zod";
-import { de, id } from "zod/v4/locales";
-const ONE_WEEK = 60*60*24*7
 
-export async function SignUp(params:SignUpParams){
-    const {uid,name,email}=params;
-    try {
-        const userRecord = await db.collection('users').doc(uid).get()
-        if(userRecord.exists){
-            return{
-                success:false,
-                message:'User already exists.Please sign in instead'
-            }
+// Session duration (1 week)
+const SESSION_DURATION = 60 * 60 * 24 * 7;
 
+// Set session cookie
+export async function setSessionCookie(idToken: string) {
+  const cookieStore = await cookies();
 
-        }
-        await db.collection('users').doc(uid).set({
-            name,email
-        })
-        return{
-            success:true,
-            message:'Account created successfully .Please Sign In '
-        }
+  // Create session cookie
+  const sessionCookie = await auth.createSessionCookie(idToken, {
+    expiresIn: SESSION_DURATION * 1000, // milliseconds
+  });
 
-    }catch(e:any){
-        console.error('Error creating a user',e);
-        if(e.code === 'auth/email-already-exists'){
-            return{
-                succes:false,
-                message:'This E-mail is already in use'
-            }
-
-        }
-        return{
-            success:false,
-            message:'Failed to create an account'
-        }
-
-    }
-    
+  // Set cookie in the browser
+  cookieStore.set("session", sessionCookie, {
+    maxAge: SESSION_DURATION,
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    path: "/",
+    sameSite: "lax",
+  });
 }
-export async function SignIn(params:SignInParams){
-    const {email,idToken} = params
-    try{
-        const userRecord = await auth.getUserByEmail(email)
-        if(!userRecord){
-            return{
-                success:false,
-                message : 'User does not exist.Create an accoungt instsead'
 
-            }
-        }
-        await setSessionCookies(idToken);
-    }catch(e){
-        console.log(e);
-        return{
-            success:false,
-            message:'failed to login into the account'
-        }
+export async function SignUp(params: SignUpParams) {
+  const { uid, name, email } = params;
+
+  try {
+    // check if user exists in db
+    const userRecord = await db.collection("users").doc(uid).get();
+    if (userRecord.exists)
+      return {
+        success: false,
+        message: "User already exists. Please sign in.",
+      };
+
+    // save user to db
+    await db.collection("users").doc(uid).set({
+      name,
+      email,
+      // profileURL,
+      // resumeURL,
+    });
+
+    return {
+      success: true,
+      message: "Account created successfully. Please sign in.",
+    };
+  } catch (error: any) {
+    console.error("Error creating user:", error);
+
+    // Handle Firebase specific errors
+    if (error.code === "auth/email-already-exists") {
+      return {
+        success: false,
+        message: "This email is already in use",
+      };
     }
 
+    return {
+      success: false,
+      message: "Failed to create account. Please try again.",
+    };
+  }
 }
-export async function setSessionCookies(idToken : string){
-    const cookieStore = await   cookies()
-    const sessionCookies = await auth.createSessionCookie(idToken,{
-        expiresIn:ONE_WEEK*1000,
-    })
-    cookieStore.set('session',sessionCookies,{
-        maxAge:ONE_WEEK,
-        httpOnly:true,
-        secure:process.env.NODE_ENV === 'production',
-        path:'/',
-        sameSite:'lax'
-         
-    })
 
+export async function SignIn(params: SignInParams) {
+  const { email, idToken } = params;
+
+  try {
+    const userRecord = await auth.getUserByEmail(email);
+    if (!userRecord)
+      return {
+        success: false,
+        message: "User does not exist. Create an account.",
+      };
+
+    await setSessionCookie(idToken);
+  } catch (error: any) {
+    console.log("");
+
+    return {
+      success: false,
+      message: "Failed to log into account. Please try again.",
+    };
+  }
 }
-export async function getCurrentuser(): Promise<User|null>{
-    const cookieStore = await cookies();
-    const sessionCookies = cookieStore.get('session')?.value;
-    if(!sessionCookies) return null;
-    try{
-        const decodeClaims = await auth.verifySessionCookie(sessionCookies,true);
-        const userRecord = await db
-        .collection('users')
-        .doc(decodeClaims.uid)
-        .get()
-        if(!userRecord.exists) return null
-        return {
-            ...userRecord.data(),
-            id:userRecord.id,
-        }as User;
 
+// Sign out user by clearing the session cookie
+export async function SignOut() {
+  const cookieStore = await cookies();
 
-    }catch(e){
-        console.log(e)
-        return null
-    }
-
+  cookieStore.delete("session");
 }
-export async function isAuthenticated(){
-    const user = await getCurrentuser()
-    return !!user;
+
+// Get current user from session cookie
+export async function getCurrentUser(): Promise<User | null> {
+  const cookieStore = await cookies();
+    console.log("SESSION COOKIE:", cookieStore.get("session"));
+
+  const sessionCookie = cookieStore.get("session")?.value;
+  if (!sessionCookie) return null;
+
+  try {
+    const decodedClaims = await auth.verifySessionCookie(sessionCookie, true);
+
+    // get user info from db
+    const userRecord = await db
+      .collection("users")
+      .doc(decodedClaims.uid)
+      .get();
+    if (!userRecord.exists) return null;
+
+    return {
+      ...userRecord.data(),
+      id: userRecord.id,
+    } as User;
+  } catch (error) {
+    console.log(error);
+
+    // Invalid or expired session
+    return null;
+  }
+}
+
+// Check if user is authenticated
+export async function isAuthenticated() {
+  const user = await getCurrentUser();
+  return !!user;
 }
